d8b545104fcb96437e6301c714f5f127824826c3
Lucas Scott <lucasluke307@gmail.com>
2023-07-24 23:31:37 -0300

    Update index.js and style.css

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+/* Resolver bug de menu em telas maiores */
+
 function novaDimensao(){
-    if (window.innerWidth >= 768){
+    if (innerWidth >= 768){
         itens.style.display = 'block'
     } else {
         itens.style.display = 'none'
     }
 }
+
+/* Mostrar menu ao clicar o botão */
 
 function clickMenu() {
     if(itens.style.display == 'block'){
@@ -12,4 +16,19 @@ function clickMenu() {
     } else {
         itens.style.display = 'block';
     }
+}
+
+
+/* Ocultar menu após clicar em uma ancora */
+
+const menuItens = itens.getElementsByTagName('a');
+
+for (let item of menuItens) {
+    item.addEventListener('click', () => {
+        if (innerWidth > 768){
+            itens.style.display = 'block';
+        } else {
+            itens.style.display = 'none';
+        }
+    });
 }
